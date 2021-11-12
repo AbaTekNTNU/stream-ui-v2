802300ff4bb75e9aefa563f64cb9fad42ae90ca6
PeterJFB <peterj.flbj@gmail.com>
2021-11-12 23:03:48 +0100

    Add boiler score window

--- src/modules/basket/score/components/scoreView.tsx
+++ src/modules/basket/score/components/scoreView.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../../store/store";
+import Score from "./Score";
 
 const getUiTime = (secondsRemainging: number): string => {
   return `${Math.floor(secondsRemainging / 60)}:${secondsRemainging % 60}`;
@@ -11,6 +12,7 @@ const ScoreView: React.FC = () => {
 
   const scoreApplication = (
     <div>
+      <Score home={scoreState.score.home} away={scoreState.score.away}></Score>
       Hjemme: {scoreState.score.home}
       Borte: {scoreState.score.away}
       Periode: {scoreState.clock.period}
