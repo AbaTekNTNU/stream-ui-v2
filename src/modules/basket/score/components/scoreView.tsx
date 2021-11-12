3c3f6b994b739549df96a8027ed3afe60f36b342
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2021-11-12 15:15:55 +0100

    feat: add clock

--- src/modules/basket/score/components/scoreView.tsx
+++ src/modules/basket/score/components/scoreView.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../../store/store";
 
+const getUiTime = (secondsRemainging: number): string => {
+  return `${Math.floor(secondsRemainging / 60)}:${secondsRemainging % 60}`;
+};
+
 const ScoreView: React.FC = () => {
   const scoreState = useSelector((state: RootState) => state.score);
 
@@ -9,6 +13,8 @@ const ScoreView: React.FC = () => {
     <div>
       Hjemme: {scoreState.score.home}
       Borte: {scoreState.score.away}
+      Periode: {scoreState.clock.period}
+      Tid: {getUiTime(scoreState.clock.secondsRemaining)}
     </div>
   );
 
